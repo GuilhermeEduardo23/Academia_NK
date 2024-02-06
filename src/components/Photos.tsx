c8f78abcb6ce7a1e399e08344f3f0c4d5facf066
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2024-02-06 18:32:25 +0000

    Alteração do caminho das imagens e mudança do caminho no componente Photos

--- src/components/Photos.tsx
+++ src/components/Photos.tsx
@@ -8,12 +8,12 @@ interface Image {
 
 const Photos = () => {
   const images: Array<Image> = [
-    {id: '1', path: './src/assets/images/motivacional1.png', alt: 'Chega de dar desculpas. Bora treinar?'},
-    {id: '2', path: './src/assets/images/motivacional2.png', alt: 'Foco, dedicação, motivação'},
-    {id: '3', path: './src/assets/images/motivacional3.png', alt: 'Trace novos objetivos para sua vida'},
-    {id: '4', path: './src/assets/images/motivacional4.png', alt: 'Conquiste o corpo dos seus sonhos.'},
-    {id: '5', path: './src/assets/images/aulaKaratê.png', alt: 'Foto do treino de Karatê'},
-    {id: '6', path: './src/assets/images/informativo.png', alt: 'Agende seu horário de treino.'},
+    {id: '1', path: '/assets/images/motivacional1.png', alt: 'Chega de dar desculpas. Bora treinar?'},
+    {id: '2', path: '/assets/images/motivacional2.png', alt: 'Foco, dedicação, motivação'},
+    {id: '3', path: '/assets/images/motivacional3.png', alt: 'Trace novos objetivos para sua vida'},
+    {id: '4', path: '/assets/images/motivacional4.png', alt: 'Conquiste o corpo dos seus sonhos.'},
+    {id: '5', path: '/assets/images/aulaKaratê.png', alt: 'Foto do treino de Karatê'},
+    {id: '6', path: '/assets/images/informativo.png', alt: 'Agende seu horário de treino.'},
   ];
 
   return (
