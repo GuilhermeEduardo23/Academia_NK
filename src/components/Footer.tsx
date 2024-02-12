814895f283be37a9891937f175c98e78112944bf
GuilhermeEduardo23 <guilhermeeduardo-17@outlook.com>
2024-02-12 17:20:48 +0000

    Lógica e responsividade no Header

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="flex items-center justify-around p-3 border-2 border-solid border-t-[#f58634] border-r-0 border-l-0 border-b-0">
+    <footer className="flex flex-col items-center justify-around p-3 border-2 border-solid border-t-[#f58634] border-r-0 border-l-0 border-b-0">
         <p>Todos os direitos reservados.</p>
         <p><span className="font-bold">Desenvolvido por:</span> Guilherme Eduardo</p>
     </footer>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { FaInstagram, FaWhatsapp } from 'react-icons/fa';
+import { IoIosMenu } from "react-icons/io";
 
 interface link {
   id: string;
@@ -13,28 +15,37 @@ const Header = () => {
     {id: '3', href: '#location', description: 'Onde Estamos'},
   ]
 
-  return (
-    <header className="text-white flex items-center justify-between p-4 mb-20 border_orange">
-        <h1 className="font-bold text-[1.8rem]">Academia N<span className="text-[#f58634]">K</span></h1>
+  const [click, setClick] = useState(false);
 
-        <nav className='flex'>
-            <ul className="flex gap-[2rem]">
-             {links.map(link => (
+  const checkClick = (): void => {
+    setClick(true);
+
+    if(click)
+      setClick(false);
+  }
+
+  return (
+    <header className="flex items-center justify-between p-4 mb-20 border_orange">
+        <h1 className="font-bold text-[1.5rem]">Academia N<span className="text-[#f58634]">K</span></h1>
+        <nav className={click ? 'flex flex-col items-center justify-center bg-red-500 md:flex' : 'hidden md:flex'}>
+          <ul className="flex gap-[2rem]">
+            {links.map(link => (
               <li key={link.id}>
                 <a href={link.href} className='links linksHover'>{link.description}</a>
               </li>
              ))}
-            </ul>
-        </nav>
-
-        <div className='flex gap-4'>
+          </ul>
+          
           <a href='https://www.instagram.com/academia_nk/' className='socialLinks bg-pink-500 links hover:bg-white hover:text-pink-500' target='_blank'>
-            <FaInstagram /> Instagram
+              <FaInstagram /> Instagram
           </a>
           <a href='https://wa.me/5564996684840' className='socialLinks bg-[#075E54] links hover:bg-white hover:text-[#075E54]' target='_blank'>
-            <FaWhatsapp /> WhatsApp
+              <FaWhatsapp /> WhatsApp
           </a>
-        </div>
+        </nav>
+        
+        <IoIosMenu className='text-3xl cursor-pointer md:hidden' onClick={checkClick}/>
+
     </header>
   )
 }
