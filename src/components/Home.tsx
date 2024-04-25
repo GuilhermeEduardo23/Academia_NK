29ac87c55ff13512827e4f5f876e2cf871b5f7e5
Guilherme Eduardo <guilhermeeduardo-17@outlook.com>
2024-04-25 22:20:51 +0100

    Alterando estilos do Header e Location

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,11 @@ const Home = () => {
     <motion.section
       initial={{ opacity: 0, scale: 0.5 }}
       animate={{ opacity: 1, scale: 1 }}
-      transition={{ duration: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
       id="home" className="text-center font-bold flex flex-col items-center justify-around text-2xl gap-10 p-4">
       <p className="leading-[50px]">"Malhar não fica mais fácil, você que fica melhor a cada treino!" 🏋🏻‍♂️🏋🏻‍♀️</p>
       <p className="leading-[50px]">"Exercícios não mudam apenas o seu corpo. Muda a sua mente, atitudes e o seu humor!" 💪🏻</p>
